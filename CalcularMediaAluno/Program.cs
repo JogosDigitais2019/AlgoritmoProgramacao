4196dafca566ca6b7096927a4dd639fa9d05ff75
apaco <alex_campos_cxm@hotmail.com>
2019-04-01 16:44:44 -0300

    Programa para calcular a média entre duas notas de um aluno.

---
+++ CalcularMediaAluno/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularMediaAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal nota01, nota02, media;
+
+            Console.WriteLine("Informe a nota 01: ");
+            nota01 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Informe a nota 02: ");
+            nota02 = Convert.ToDecimal(Console.ReadLine());
+
+            media = (nota01 + nota02) / 2;
+
+            Console.WriteLine("A média do aluno é " + media);
+
+            Console.ReadKey();
+        }
+    }
+}

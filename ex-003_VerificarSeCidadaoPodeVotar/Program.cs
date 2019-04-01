a9134a55b40a8cd811941c8346d6fcaca77cc4e8
apaco <alex_campos_cxm@hotmail.com>
2019-04-01 17:17:00 -0300

    Finalizado programa para verificar se uma pessoa pode ou não votar.

---
+++ ex-003_VerificarSeCidadaoPodeVotar/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_003_VerificarSeCidadaoPodeVotar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeCompleto;
+            int anoNascimento;
+            int idade;
+
+            Console.WriteLine("Informe o nome completo: ");
+            nomeCompleto = Console.ReadLine();
+
+            Console.WriteLine("Informe o ano de nascimento: ");
+            anoNascimento = Convert.ToInt32(Console.ReadLine());
+
+            idade = 2019 - anoNascimento;
+
+            Console.WriteLine("O cidadão possui " + idade + " anos.");
+
+            if (idade >= 16)
+            {
+                Console.WriteLine("Pode votar.");
+            }
+            else
+            {
+                Console.WriteLine("NÃO pode votar.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
